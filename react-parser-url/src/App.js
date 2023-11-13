d5c57381022c1aba153bba2c3e37bf0bd309036b
wangchunji <2686521696@qq.com>
2023-11-13 18:13:28 +0800

    ✨ feat(react-parser-url): URL 解析函数
    1、从URL对象中提取各部分信息（protocol，hostname，port，pathname），2、提取查询参数，
    3、提取哈希部分

--- react-parser-url/src/App.js
+++ react-parser-url/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+// 默认的 URL 解析结果
 const defaultParserResult = {
   protocol: '',
   hostname: '',
@@ -10,38 +11,69 @@ const defaultParserResult = {
   hash: ''
 }
 
+// URL 解析函数
 const parserUrl = (url) => {
-  // 你的实现
-  return defaultParserResult;
+  const result = { ...defaultParserResult };
+
+  try {
+    const urlObj = new URL(url);
+
+    // 从 URL 对象中提取各部分信息
+    result.protocol = urlObj.protocol;
+    result.hostname = urlObj.hostname;
+    result.port = urlObj.port;
+    result.pathname = urlObj.pathname;
+
+    // 提取查询参数
+    const params = new URLSearchParams(urlObj.search);
+    params.forEach((value, key) => {
+      result.params[key] = value;
+    });
+
+    // 提取哈希部分
+    result.hash = urlObj.hash;
+  } catch (error) {
+    console.error('无效的 URL:', error.message);
+  }
+
+  return result;
 };
-
-// 测试用例
-parserUrl('https://baidu.com:443/s?wd=hello');
-// 输出结果：{ protocol: 'https:', hostname: 'baidu.com', port: '443', pathname: '/s', params: { wd: 'hello' },  hash: '' }
-
 
 function App() {
   const [result, setResult] = useState(defaultParserResult);
 
   useEffect(() => {
+    const handleParseUrl = () => {
+      const inputUrl = document.querySelector('input').value;
+      setResult(parserUrl(inputUrl));
+    };
+
     const onKeyDown = (e) => {
       if (e.keyCode === 13) {
-        console.log('这里 处理 Enter 事件');
-        setResult(defaultParserResult);
+        console.log('在这里处理 Enter 事件');
+        handleParseUrl();
       }
     }
+
+    const parseButton = document.getElementById('J-parserBtn');
+    parseButton.addEventListener('click', handleParseUrl, false);
     document.addEventListener('keydown', onKeyDown, false);
+
+    return () => {
+      parseButton.removeEventListener('click', handleParseUrl, false);
+      document.removeEventListener('keydown', onKeyDown, false);
+    };
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>请实现 App.js 中 parserUrl 方法，当用户在输入框中输入url时，</div>
-        <div>点击解析按钮（或者按 enter 快捷键）能够识别出 url 各个组成部分</div>
-        <div>并将结果渲染在页面上（tips: 请尽可能保证 parserUrl 的健壮性和完备性）</div>
+        <div>请实现 App.js 中 parserUrl 方法，当用户在输入框中输入 URL 时，</div>
+        <div>点击解析按钮（或者按 Enter 快捷键）能够识别出 URL 各个组成部分</div>
+        <div>并将结果渲染在页面上（提示: 请尽可能保证 parserUrl 的健壮性和完备性）</div>
       </header>
       <section className="App-content">
-        <input type="text" placeholder="请输入 url 字符串" />
+        <input type="text" placeholder="请输入 URL 字符串" />
         <button id="J-parserBtn">解析</button>
       </section>
       <section className="App-result">
